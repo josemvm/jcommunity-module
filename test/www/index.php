8e5aacf4a9e8bc056be052a4be934bffb7302d68
Laurent Jouanneau <laurent@jelix.org>
2009-03-30 20:27:30 +0000

    Updated jelix to its latest trunk version
    --HG--
    extra : convert_revision : svn%3A732d0b39-3f36-0410-87bd-fe2e92cfd321/trunk%4033

--- test/www/index.php
+++ test/www/index.php
@@ -7,8 +7,6 @@
 * @copyright
 */
 
-require_once ('../../lib/jelix/init.php');
-
 require_once ('../application.init.php');
 
 require_once (JELIX_LIB_CORE_PATH.'request/jClassicRequest.class.php');
