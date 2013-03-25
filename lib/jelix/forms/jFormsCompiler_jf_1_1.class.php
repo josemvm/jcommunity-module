442a9faa5ae1b7e903ef40d58d13cd9bca050f6b
Laurent Jouanneau <laurent@jelix.org>
2013-03-25 11:40:53 +0100

    Update test lib to jelix-1.5

--- lib/jelix/forms/jFormsCompiler_jf_1_1.class.php
+++ lib/jelix/forms/jFormsCompiler_jf_1_1.class.php
@@ -165,7 +165,7 @@ class jFormsCompiler_jf_1_1 extends jFormsCompiler_jf_1_0 {
 
     protected function generateHtmleditor(&$source, $control, &$attributes) {
         if (isset($attributes['xhtml'])) {
-            $source[] = '$ctrl->datatype= new jDatatypeHtml('.($attributes['xhtml'] == 'true'?'true':'').', true);';
+            $source[] = '$ctrl->datatype= new jDatatypeHtml('.($attributes['xhtml'] == 'true'?'true':'false').', true);';
             unset($attributes['xhtml']);
         }
 
@@ -200,6 +200,13 @@ class jFormsCompiler_jf_1_1 extends jFormsCompiler_jf_1_0 {
     protected function generateCaptcha(&$source, $control, &$attributes) {
         $this->readLabel($source, $control, 'captcha');
         $this->readHelpHintAlert($source, $control);
+        return false;
+    }
+
+    protected function generateButton(&$source, $control, &$attributes) {
+        $this->attrDefaultvalue($source, $attributes);
+        $this->readLabel($source, $control, 'button');
+        //$this->readHelpHintAlert($source, $control);
         return false;
     }
 
@@ -282,7 +289,7 @@ class jFormsCompiler_jf_1_1 extends jFormsCompiler_jf_1_0 {
         foreach($xml->children() as $ctrltype=>$control){
             if(in_array($ctrltype, $ignore))
                 continue;
-            if(!in_array($ctrltype, array('input','textarea', 'output','checkbox','checkboxes','radiobuttons',
+            if(!in_array($ctrltype, array('input','textarea', 'output','checkbox','checkboxes','radiobuttons','button',
                         'menulist','listbox','secret', 'upload', 'hidden','htmleditor','date','datetime','wikieditor'))) {
                 throw new jException('jelix~formserr.control.not.allowed',array($ctrltype, $controltype,$this->sourceFile));
             }
