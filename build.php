5ebb2bb598f223dd8ab759332b89d9017f4fd75c
Laurent Jouanneau <laurent@jelix.org>
2013-03-25 11:26:40 +0100

    change the build

--- build.php
+++ build.php
@@ -2,20 +2,8 @@
 
 
 
-function buildPackage($dir, $name) {
-    $xml = simplexml_load_file($dir.'module.xml');
-    $version = (string)$xml->info[0]->version;
-
-    echo $version;
-    echo "\n";
-    
-    chdir($dir.'../');
-    exec('zip -r ../'.$name.'-'.$version.'.zip '.$name);
-}
-
-
-buildPackage(__DIR__.'/modules/jcommunity/', 'jcommunity');
-buildPackage(__DIR__.'/modules/jcommunity_phorum/', 'jcommunity_phorum');
-buildPackage(__DIR__.'/modules/jmessenger/', 'jmessenger');
+$xml = simplexml_load_file(__DIR__.'/modules/jcommunity/module.xml');
+$version = (string)$xml->info[0]->version;
+exec('zip -r jcommunity-'.$version.'.zip modules/');
 
 
