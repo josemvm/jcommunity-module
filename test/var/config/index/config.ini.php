5f399241d3e9a0405d173f2f120ba3041cdc3f39
Laurent Jouanneau <laurent@jelix.org>
2013-03-25 11:50:54 +0100

    Update code of jCommunity for Jelix 1.5

--- test/var/config/index/config.ini.php
+++ test/var/config/index/config.ini.php
@@ -5,3 +5,10 @@ startModule=test
 startAction="default:index"
 
 
+
+[coordplugins]
+jacl2=1
+[coordplugin_jacl2]
+on_error=2
+error_message="jacl2~errors.action.right.needed"
+on_error_action="jelix~error:badright"
