5503d2943d3717bfc0aea54ccc3be38334cdff94
Laurent Jouanneau <dev@ljouanneau.com>
2015-06-25 00:01:02 +0200

    Test vm: reset script should delete jcommunity tables

--- test/vagrant/reset_app.sh
+++ test/vagrant/reset_app.sh
@@ -9,6 +9,9 @@ APPHOSTNAME2=""
 source $VAGRANTDIR/jelixapp/system.sh
 
 resetJelixMysql $APPNAME root jelix
+
+mysql -u root -pjelix -e "drop table if exists community_users;drop table if exists jmessenger;" $APPNAME;
+
 resetJelixInstall $APPDIR
 
 initapp $APPDIR
